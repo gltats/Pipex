a9afc3a4c14eea6ebc2219ec230bf159be873b3b
Tatiana Sofia Gomes Lima <tgomes-l@c2s13d69.42wolfsburg.de>
2023-02-01 17:29:24 +0100

    understanding child and parent process

--- main.c
+++ main.c
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tgomes-l <tgomes-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/29 16:06:28 by tgomes-l          #+#    #+#             */
-/*   Updated: 2023/02/01 17:27:35 by tgomes-l         ###   ########.fr       */
+/*   Created: 2023/02/01 14:37:50 by tgomes-l          #+#    #+#             */
+/*   Updated: 2023/02/01 17:28:26 by tgomes-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#include "pipex.h"
+//envp -> enviromental variable, example $PATH
 
-# include <stdio.h>
-# include <sys/wait.h> //for pid_t
-# include <unistd.h>
-
-int pipex(int argc, char *argv[], char *envp[]);
-#endif
+int main(int argc, char *argv[], char *envp[])
+{
+	pipex(argc, argv, envp);
+	return(0);
+}
--- pipex.c
+++ pipex.c
@@ -6,23 +6,80 @@
 /*   By: tgomes-l <tgomes-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 16:03:42 by tgomes-l          #+#    #+#             */
-/*   Updated: 2023/02/01 12:05:28 by tgomes-l         ###   ########.fr       */
+/*   Updated: 2023/02/01 17:27:03 by tgomes-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-//envp -> enviromental variable, exaple $PATH
-int main(int argc,char* argv[], char *envp[])
+//PID -> process identifier, is a unique numerical value assigned by an 
+//operating system to each process that is created on a computer
+//waitpid -> is used by a parent process to wait for the termination 
+//of one of its child processes.
+int pipex(int argc, char *argv[], char *envp[])
 {
-	int i;
+	int	fd[2];
+	pid_t pid1;
+	pid_t pid2;
+	int ret;
+	int status;
 	
-	i = 0;
-	printf("Program Name Is: %s\n",argv[0]);
-	if (argc == 1)
-		printf("No Extra Command Line Argument Passed Other Than Program Name\n");
-	else if (argc == 2)
-	{		
-		printf("Amazing, your argv is -> %s\n",argv[1]);
+	if (argc != 5)
+	{
+		printf("Error: incorrect number of arguments\n");
+		return (1);
 	}
-	return 0;
+	if (pipe(fd) == -1)
+	{
+		perror("ERROR");
+		return (1);
+	}
+		
+	pid1 = fork();
+    if (pid1 == -1) 
+	{
+		perror("ERROR");
+		return (1);
+	}
+	if (pid1 == 0) 
+	{
+		printf("I am the child process, my PID is %d\n", getpid());
+		return(0);
+	}
+	else
+	{
+		ret = waitpid(pid1, &status, 0);
+		if (ret == -1) 
+		{
+			perror("waitpid");
+			return (1);
+		}
+		printf("I am the parent process, my first child's PID is %d\n", pid1);
+		printf("My first child exit status is %d\n", status);
+	}
+	
+	pid2 = fork();
+	if (pid2 == -1)
+	{
+		perror("ERROR");
+		return (1);
+	}
+	if (pid2 == 0) 
+	{
+		printf("I am the child process, my PID is %d\n", getpid());
+		return(0);
+	}
+	else 
+	{
+		ret = waitpid(pid2, &status, 0);
+		if (ret == -1) 
+		{
+			perror("waitpid");
+			return(1);
+		}
+		printf("I am the parent process, my second child's PID is %d\n", pid2);
+		printf("My second child exit status is %d\n", status);
+	}
+	close(fd[0]);
+	close(fd[1]);
+	return(0);
 }
